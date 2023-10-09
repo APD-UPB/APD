b9ef83f1b848eba4a994ae6d149f06ebdbc292fb
Dorinel Filip <dorinel.filip@upb.ro>
2023-10-09 18:08:00 +0300

    Consistent identation is a must

--- laboratoare/lab01/add_serial.c
+++ laboratoare/lab01/add_serial.c
@@ -6,44 +6,44 @@
     schelet pentru exercitiul 5
 */
 
-int* arr;
+int *arr;
 int array_size;
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        perror("Specificati dimensiunea array-ului\n");
-        exit(-1);
+  if (argc < 2) {
+    perror("Specificati dimensiunea array-ului\n");
+    exit(-1);
+  }
+
+  array_size = atoi(argv[1]);
+
+  arr = malloc(array_size * sizeof(int));
+  for (int i = 0; i < array_size; i++) {
+    arr[i] = i;
+  }
+
+  for (int i = 0; i < array_size; i++) {
+    printf("%d", arr[i]);
+    if (i != array_size - 1) {
+      printf(" ");
+    } else {
+      printf("\n");
     }
+  }
 
-    array_size = atoi(argv[1]);
+  // TODO: aceasta operatie va fi paralelizata
+  for (int i = 0; i < array_size; i++) {
+    arr[i] += 100;
+  }
 
-    arr = malloc(array_size * sizeof(int));
-    for (int i = 0; i < array_size; i++) {
-        arr[i] = i;
+  for (int i = 0; i < array_size; i++) {
+    printf("%d", arr[i]);
+    if (i != array_size - 1) {
+      printf(" ");
+    } else {
+      printf("\n");
     }
+  }
 
-    for (int i = 0; i < array_size; i++) {
-        printf("%d", arr[i]);
-        if (i != array_size - 1) {
-            printf(" ");
-        } else {
-            printf("\n");
-        }
-    }
-
-    // TODO: aceasta operatie va fi paralelizata
-  	for (int i = 0; i < array_size; i++) {
-        arr[i] += 100;
-    }
-
-    for (int i = 0; i < array_size; i++) {
-        printf("%d", arr[i]);
-        if (i != array_size - 1) {
-            printf(" ");
-        } else {
-            printf("\n");
-        }
-    }
-
-  	pthread_exit(NULL);
+  pthread_exit(NULL);
 }
--- laboratoare/lab01/example.c
+++ laboratoare/lab01/example.c
@@ -5,36 +5,36 @@
 #define NUM_THREADS 2
 
 void *f(void *arg) {
-  	long id = *(long*)arg;
-  	printf("Hello World din thread-ul %ld!\n", id);
-  	pthread_exit(NULL);
+  long id = *(long *)arg;
+  printf("Hello World din thread-ul %ld!\n", id);
+  pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
-	pthread_t threads[NUM_THREADS];
-  	int r;
-  	long id;
-  	void *status;
-	long ids[NUM_THREADS];
+  pthread_t threads[NUM_THREADS];
+  int r;
+  long id;
+  void *status;
+  long ids[NUM_THREADS];
 
-  	for (id = 0; id < NUM_THREADS; id++) {
-		ids[id] = id; 
-		r = pthread_create(&threads[id], NULL, f, &ids[id]);
+  for (id = 0; id < NUM_THREADS; id++) {
+    ids[id] = id;
+    r = pthread_create(&threads[id], NULL, f, &ids[id]);
 
-		if (r) {
-	  		printf("Eroare la crearea thread-ului %ld\n", id);
-	  		exit(-1);
-		}
-  	}
+    if (r) {
+      printf("Eroare la crearea thread-ului %ld\n", id);
+      exit(-1);
+    }
+  }
 
-  	for (id = 0; id < NUM_THREADS; id++) {
-		r = pthread_join(threads[id], &status);
+  for (id = 0; id < NUM_THREADS; id++) {
+    r = pthread_join(threads[id], &status);
 
-		if (r) {
-	  		printf("Eroare la asteptarea thread-ului %ld\n", id);
-	  		exit(-1);
-		}
-  	}
+    if (r) {
+      printf("Eroare la asteptarea thread-ului %ld\n", id);
+      exit(-1);
+    }
+  }
 
-  	pthread_exit(NULL);
+  pthread_exit(NULL);
 }
