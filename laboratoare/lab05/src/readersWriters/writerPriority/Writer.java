3a49a78d83d3e7e136acad5444ce27cade70c682
Dorinel Filip <dorinel.filip@upb.ro>
2023-11-08 01:40:52 +0200

    [lab5]: Small fixes for Writer skel

--- laboratoare/lab05/src/readersWriters/writerPriority/Writer.java
+++ laboratoare/lab05/src/readersWriters/writerPriority/Writer.java
@@ -8,9 +8,9 @@ public class Writer extends Thread {
     private double start_time;
     private double completion_time;
 
-    public Writer(int id, int number_of_reads, long wait_time_ms, ReaderWriterSharedVars shared_vars) {
+    public Writer(int id, int number_of_writes, long wait_time_ms, ReaderWriterSharedVars shared_vars) {
         this.id = id;
-        this.number_of_writes = number_of_reads;
+        this.number_of_writes = number_of_writes;
         this.wait_time_ms = wait_time_ms;
         this.shared_vars = shared_vars;
     }
@@ -35,8 +35,20 @@ public class Writer extends Thread {
     public void write() {
         // This is an example of updating the value based on the previous one.
         // Reading should also be OK for writers, since they have exclusive access.
+
+        // Compute the new value
         int new_value = shared_vars.shared_value + 3;
+
+        // Print an informative message (for debug)
+
         System.out.println(Utils.get_current_time_str() + " | Writer " + this.id + " writes " + shared_vars.shared_value);
         this.shared_vars.shared_value = new_value;
+
+        // sleep wait_time_ms to simulate a longer write operation
+        try {
+            sleep(wait_time_ms);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
