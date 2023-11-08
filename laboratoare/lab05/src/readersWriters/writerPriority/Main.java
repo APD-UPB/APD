6080329413777e45c2fd9c6bbaa90750d66c2049
Dorinel Filip <dorinel.filip@upb.ro>
2023-11-08 02:10:59 +0200

    More realistic number of writes (compared to reads).

--- laboratoare/lab05/src/readersWriters/writerPriority/Main.java
+++ laboratoare/lab05/src/readersWriters/writerPriority/Main.java
@@ -5,13 +5,13 @@ import java.util.List;
 // Hint: All the TODOs are in Reader, Writer and ReaderWriterSharedVars
 
 public class Main {
-    public static final int WRITERS = 6;
+    public static final int WRITERS = 4;
     public static final int READERS = 8;
     public static final long readers_wait_time_ms = 100;
     public static final long writers_wait_time_ms = 100;
 
     public static final int number_of_reads_per_thread = 100;
-    public static final int number_of_writes_per_thread = 100;
+    public static final int number_of_writes_per_thread = 20;
 
     public static final int initial_shared_value = 10;
 
@@ -45,14 +45,14 @@ public class Main {
             Reader reader = (Reader) thread;
             System.out.print(reader.getCompletion_time() + " ");
         }
-        System.out.println("");
+        System.out.println();
 
         System.out.println("Writers completion times: ");
         for(Thread thread: writers) {
             Writer writer = (Writer) thread;
             System.out.print(writer.getCompletion_time() + " ");
         }
-        System.out.println("");
+        System.out.println();
 
         int expected_final_value = initial_shared_value + 3 * number_of_writes_per_thread * WRITERS;
         System.out.println("Final value of Shared Var is " + shared_vars.shared_value + " (expected: " + expected_final_value + ")" );
